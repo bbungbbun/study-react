92207fa4a4adfb745b0f59cc40346ff24b671f0c
bbungbbun <tnqkrdbsl@naver.com>
2023-09-03 23:15:51 +0900

    input 값 받기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home"; // 파일명 시작을 대문자로 하면 오류생김
 import About from "./pages/about";
 import Counter from "./pages/counter";
+import Input from "./pages/input";
 
 
 function App() {
@@ -13,12 +14,14 @@ function App() {
         <Link to="/">Home</Link> | 
         <Link to="/about">about</Link> |
         <Link to="/counter">counter</Link> |
+        <Link to="/input">input</Link> |
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/counter" element={<Counter />} />
+        <Route path="/input" element={<Input />} />
       </Routes>
     </div>
   );
